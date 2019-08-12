36ebd95e2c6b7cc8fc9ead6139d015555d9144ab
takeshiyamamoto <takeshi.yamamoto@hotmail.com>
2019-08-12 16:36:29 +0900

    Add files via upload

--- untitled.py
+++ untitled.py
@@ -2,33 +2,34 @@
 #%%
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 #%%
 def main():
-    s = 100.0
-    r = 0.01
-    q = 0.01
-    v = 0.2
-    t = 1.0
-    trial = 1000000
-    np.random.seed(0)
-    z = np.random.standard_normal(trial)
-    f = pd.DataFrame(s * np.exp((r - q - 0.5 * v ** 2) * t + v * np.sqrt(t) * z))
-    pl = f - s
-    print(pl)
-    
+    # settings
+    var_window = 250
+    # read data series
     price = pd.read_csv('MUFG.csv', index_col='Date')
     returns = price['Adj Close'].pct_change()
-    diffs = price['Adj Close'].diff()
-
-    total_obs_days = returns.count()
-    obs_days = 252 * 3
-    for i in range(total_obs - obs_days):
-        
-    
-    pass
-
-def percentile()
+    returns.plot()
+    plt.show()
+    # calculate VaRs
+    ret_pos = returns.rolling(window=var_window).quantile(0.99)
+    ret_neg = returns.rolling(window=var_window).quantile(0.01)
+    ret_std = returns.rolling(window=var_window).std() * norm.ppf(0.99)
+    ret_pos.plot()
+    ret_neg.plot()
+    ret_std.plot()
+    plt.show()
+    # count outliers
+    outliers_pos = np.where(ret_pos[var_window:] - returns[var_window:] < 0, 1, 0)
+    outliers_neg = np.where(ret_neg[var_window:] - returns[var_window:] > 0, 1, 0)
+    sum_pos = pd.DataFrame(outliers_pos).rolling(window=var_window).sum()
+    sum_neg = pd.DataFrame(outliers_neg).rolling(window=var_window).sum()
+    sum_pos.plot()
+    sum_neg.plot()
+    plt.show()
 
 #%%
 if __name__ == "__main__":
